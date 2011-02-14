bb7570f239ff7118be58a4a767576261e1e05914
Cam Linke <cam.linke@gmail.com>
2011-02-13 17:08:30 -0700

    changed hits to kill count

--- Classes/ViewControllers/AttackViewController.m
+++ Classes/ViewControllers/AttackViewController.m
@@ -23,8 +23,8 @@
 
 #define NONSHAKE_DELTA 0.4
 #define SHAKE_DELTA 2.0
-#define HITS_TO_KILL 3
-#define HITS_TO_FINISH_HIM 2
+#define HITS_TO_KILL 15
+#define HITS_TO_FINISH_HIM 5
 
 - (IBAction)slapButton {
 	[self slap];
